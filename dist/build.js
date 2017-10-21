7e81559ec251c3c8fc40bada2b040f7b51125761
Ben Foxall <benfoxall@gmail.com>
2017-10-21 23:54:44 +0100

    Build commands + built file

---
+++ dist/build.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.ringingtones = {})));
+}(this, (function (exports) { 'use strict';
+
+var ringEU = function (audioCtx) {
+  throw new Error('not implemented')
+};
+
+exports.ringEU = ringEU;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
