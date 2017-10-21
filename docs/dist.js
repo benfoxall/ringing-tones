f5db7fd8decdce0bb4023c9a0373af2b050d3af1
Ben Foxall <benfoxall@gmail.com>
2017-10-22 00:44:06 +0100

    Initial (hacky) implementation

--- docs/dist.js
+++ docs/dist.js
@@ -7,13 +7,36 @@
 var ringEU = function (audioCtx) {
 
   var oscillator = audioCtx.createOscillator();
+  oscillator.frequency.value = 425;
   var gainNode = audioCtx.createGain();
 
   oscillator.connect(gainNode);
 
   oscillator.start();
 
-  return gainNode
+  gainNode.gain.value = 0;
+
+  var ring = function () {
+    var now = audioCtx.currentTime;
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.03);
+    gainNode.gain.linearRampToValueAtTime(1, now + 1);
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.03);
+  };
+
+  ring();
+
+  var interval = setInterval(ring, 4000);
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: function () {
+      var args = [], len = arguments.length;
+      while ( len-- ) args[ len ] = arguments[ len ];
+
+      clearInterval(interval);  
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
 };
 
 exports.ringEU = ringEU;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 export const ringEU = audioCtx => {
 
   const oscillator = audioCtx.createOscillator()
+  oscillator.frequency.value = 425
   const gainNode = audioCtx.createGain()
 
   oscillator.connect(gainNode)
 
   oscillator.start()
 
-  return gainNode
+  gainNode.gain.value = 0
+
+  const ring = () => {
+    const now = audioCtx.currentTime
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.03)
+    gainNode.gain.linearRampToValueAtTime(1, now + 1)
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.03)
+  }
+
+  ring()
+
+  const interval = setInterval(ring, 4000)
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: (...args) => {
+      clearInterval(interval)  
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
 }
