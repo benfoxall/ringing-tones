33d5c3c1ffb3ea5242224daeb0a5a209adc4e560
Ben Foxall <benfoxall@gmail.com>
2017-10-22 01:08:26 +0100

    Japan (todo: finish)

--- docs/dist.js
+++ docs/dist.js
@@ -124,9 +124,51 @@ var ringUK = function (audioCtx) {
   }
 };
 
+
+var ringJP = function (audioCtx) {
+
+  var oscillator = audioCtx.createOscillator();
+  oscillator.frequency.value = 400;
+
+  var gainNode = audioCtx.createGain();
+
+  oscillator.connect(gainNode);
+
+  oscillator.start();
+
+  gainNode.gain.value = 0;
+
+  var ring = function () {
+    var now = audioCtx.currentTime;
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.01);
+    gainNode.gain.linearRampToValueAtTime(1, now + 1);
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.01);
+
+    /* TODO:
+    The tone has a frequency of 400 ± 20 Hz, and the amplitude modulation is 15 to 20Hz
+    */
+  };
+
+  ring();
+
+  var interval = setInterval(ring, 3000);
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: function () {
+      var args = [], len = arguments.length;
+      while ( len-- ) args[ len ] = arguments[ len ];
+
+      clearInterval(interval);
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+};
+
 exports.ringEU = ringEU;
 exports.ringUS = ringUS;
 exports.ringUK = ringUK;
+exports.ringJP = ringJP;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- src/main.js
+++ src/main.js
@@ -108,3 +108,41 @@ export const ringUK = audioCtx => {
     }
   }
 }
+
+
+export const ringJP = audioCtx => {
+
+  const oscillator = audioCtx.createOscillator()
+  oscillator.frequency.value = 400
+
+  const gainNode = audioCtx.createGain()
+
+  oscillator.connect(gainNode)
+
+  oscillator.start()
+
+  gainNode.gain.value = 0
+
+  const ring = () => {
+    const now = audioCtx.currentTime
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.01)
+    gainNode.gain.linearRampToValueAtTime(1, now + 1)
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.01)
+
+    /* TODO:
+    The tone has a frequency of 400 ± 20 Hz, and the amplitude modulation is 15 to 20Hz
+    */
+  }
+
+  ring()
+
+  const interval = setInterval(ring, 3000)
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: (...args) => {
+      clearInterval(interval)
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+}
