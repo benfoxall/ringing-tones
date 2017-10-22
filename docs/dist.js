d033901d3a9860a54263e8fd7a2edab5300b0fae
Ben Foxall <benfoxall@gmail.com>
2017-10-22 01:00:24 +0100

    UK & US

--- docs/dist.js
+++ docs/dist.js
@@ -33,13 +33,100 @@ var ringEU = function (audioCtx) {
       var args = [], len = arguments.length;
       while ( len-- ) args[ len ] = arguments[ len ];
 
-      clearInterval(interval);  
+      clearInterval(interval);
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+};
+
+// TODO - this is canada too… hmm not sure of the best way to do this
+var ringUS = function (audioCtx) {
+
+  var oscillator = audioCtx.createOscillator();
+  oscillator.frequency.value = 440;
+  var oscillator2 = audioCtx.createOscillator();
+  oscillator2.frequency.value = 480;
+
+  var gainNode = audioCtx.createGain();
+
+  oscillator.connect(gainNode);
+  oscillator2.connect(gainNode);
+
+  oscillator.start();
+
+  gainNode.gain.value = 0;
+
+  var ring = function () {
+    var now = audioCtx.currentTime;
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.03);
+    gainNode.gain.linearRampToValueAtTime(1, now + 2);
+    gainNode.gain.linearRampToValueAtTime(0, now + 2.03);
+  };
+
+  ring();
+
+  var interval = setInterval(ring, 4000);
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: function () {
+      var args = [], len = arguments.length;
+      while ( len-- ) args[ len ] = arguments[ len ];
+
+      clearInterval(interval);
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+};
+
+
+// AND Republic of Ireland, Australia, New Zealand, and many Commonwealth nations
+var ringUK = function (audioCtx) {
+
+  var oscillator = audioCtx.createOscillator();
+  oscillator.frequency.value = 400;
+  var oscillator2 = audioCtx.createOscillator();
+  oscillator2.frequency.value = 450;
+
+  var gainNode = audioCtx.createGain();
+
+  oscillator.connect(gainNode);
+  oscillator2.connect(gainNode);
+
+  oscillator.start();
+
+  gainNode.gain.value = 0;
+
+  var ring = function () {
+    var now = audioCtx.currentTime;
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.01);
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.4);
+    gainNode.gain.linearRampToValueAtTime(0, now + 0.41);
+    gainNode.gain.linearRampToValueAtTime(0, now + 0.6);
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.61);
+    gainNode.gain.linearRampToValueAtTime(1, now + 1);
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.01);
+  };
+
+  ring();
+
+  var interval = setInterval(ring, 3000);
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: function () {
+      var args = [], len = arguments.length;
+      while ( len-- ) args[ len ] = arguments[ len ];
+
+      clearInterval(interval);
       return gainNode.disconnect.apply(gainNode, args)
     }
   }
 };
 
 exports.ringEU = ringEU;
+exports.ringUS = ringUS;
+exports.ringUK = ringUK;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- src/main.js
+++ src/main.js
@@ -24,7 +24,86 @@ export const ringEU = audioCtx => {
   return {
     connect: gainNode.connect.bind(gainNode),
     disconnect: (...args) => {
-      clearInterval(interval)  
+      clearInterval(interval)
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+}
+
+// TODO - this is canada too… hmm not sure of the best way to do this
+export const ringUS = audioCtx => {
+
+  const oscillator = audioCtx.createOscillator()
+  oscillator.frequency.value = 440
+  const oscillator2 = audioCtx.createOscillator()
+  oscillator2.frequency.value = 480
+
+  const gainNode = audioCtx.createGain()
+
+  oscillator.connect(gainNode)
+  oscillator2.connect(gainNode)
+
+  oscillator.start()
+
+  gainNode.gain.value = 0
+
+  const ring = () => {
+    const now = audioCtx.currentTime
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.03)
+    gainNode.gain.linearRampToValueAtTime(1, now + 2)
+    gainNode.gain.linearRampToValueAtTime(0, now + 2.03)
+  }
+
+  ring()
+
+  const interval = setInterval(ring, 4000)
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: (...args) => {
+      clearInterval(interval)
+      return gainNode.disconnect.apply(gainNode, args)
+    }
+  }
+}
+
+
+// AND Republic of Ireland, Australia, New Zealand, and many Commonwealth nations
+export const ringUK = audioCtx => {
+
+  const oscillator = audioCtx.createOscillator()
+  oscillator.frequency.value = 400
+  const oscillator2 = audioCtx.createOscillator()
+  oscillator2.frequency.value = 450
+
+  const gainNode = audioCtx.createGain()
+
+  oscillator.connect(gainNode)
+  oscillator2.connect(gainNode)
+
+  oscillator.start()
+
+  gainNode.gain.value = 0
+
+  const ring = () => {
+    const now = audioCtx.currentTime
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.01)
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.4)
+    gainNode.gain.linearRampToValueAtTime(0, now + 0.41)
+    gainNode.gain.linearRampToValueAtTime(0, now + 0.6)
+    gainNode.gain.linearRampToValueAtTime(1, now + 0.61)
+    gainNode.gain.linearRampToValueAtTime(1, now + 1)
+    gainNode.gain.linearRampToValueAtTime(0, now + 1.01)
+  }
+
+  ring()
+
+  const interval = setInterval(ring, 3000)
+
+  return {
+    connect: gainNode.connect.bind(gainNode),
+    disconnect: (...args) => {
+      clearInterval(interval)
       return gainNode.disconnect.apply(gainNode, args)
     }
   }
